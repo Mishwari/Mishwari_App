64e514f0d764ce78a38fd299b5884772c71a717e
Husni Abad <husni.abad.97@gmail.com>
2023-10-09 02:46:47 +0530

    add BusList page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'swiper/css';
+  
+
+
+
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
