7f843c5e4ca3e7b150b0f188e1ca22f698bc0905
HailSaeed <54307479+HailSaeed@users.noreply.github.com>
2023-10-11 13:10:54 +0530

    from hail branch within cities selection and method been added

--- src/components/Booking_Board/Between_Cities.tsx
+++ src/components/Booking_Board/Between_Cities.tsx
@@ -1,29 +1,80 @@
-import React from 'react'
-import Image from 'next/image'
+import React, { useState } from 'react'
+import Testdata from '../testdata.json'
+import Image from 'next/image';
 function Between_Cities() {
+
+  const [stateName, setStateName] = useState<string[]>([]);
+  const [Countrystate, setCountrystate] = useState([]);
+  const [selectCity, setSelectCity] = useState<string>('');
+  const [selectFrom, setSelectFrom] = useState<string>('');
+  const [selectTo, setSelectTo] = useState<string>('');
+  const [selectedMethods, setselectedMethods] = useState<string[]>([])
+
+
+
+  const OnTarvelModeSelected = (e: any) => {
+    const selectedvalue = e.target.value;
+    if (e.target.checked) {
+      setselectedMethods((prevValue: any) => [...prevValue, selectedvalue])
+    } else {
+      setselectedMethods((prevValue) => prevValue.filter((item) => item !== selectedvalue))
+    }
+  }
+
+  console.log(selectedMethods)
+  const handleSubmit = () => {
+    alert("Selected Country:" + " " + selectCity + +"," + " From: " + " " + selectFrom + ", " + "To: " + " " + selectTo + " ," + "Travel Method by:" + " " + selectedMethods);
+  };
   return (
-    <div>
-      <div className='flex gap-x-3'>
-          <div className='border-2 border-[#005687] bg-[#cbd8df] w-20 h-14 rounded-lg overflow-hidden'>
-            <Image alt='bike' src='/plane.svg' height={10} width={10} className=' h-16 w-64 object-center '/>
-          </div>
-
-          <div className='border-2 border-[#005687] bg-[#cbd8df] w-20 h-14 rounded-lg overflow-hidden'>
-            <Image alt='bike' src='/bus.svg' height={10} width={10} className=' h-16  mb-6 w-20 object-center '/>
-          </div>
-
-          <div className='border-2 border-[#005687] bg-[#cbd8df] w-20 h-14 rounded-lg overflow-hidden'>
-            <Image alt='bike' src='/bulka_bus.svg' height={1} width={1} className=' h-20 pt-4 w-64 object-center '/>
-          </div>
-
-          <div className='border-2 border-[#005687] bg-[#cbd8df] w-20 h-14 rounded-lg overflow-hidden'>
-            <Image alt='bike' src='/car.svg' height={10} width={10} className=' h-16 w-64 object-center '/>
-          </div>
-
-          <div className='border-2 border-[#005687] bg-[#cbd8df] w-20 h-14 rounded-lg overflow-hidden'>
-            <Image alt='bike' src='/bike.svg' height={10} width={10} className=' h-16 w-64 object-center '/>
-          </div>
+    <div className='text-[#101010] px-4'>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <h1 className='font-semibold text-md text-[#676767] pt-6'>From</h1>
+          <select value={selectFrom} onChange={
+            (e: React.ChangeEvent<HTMLSelectElement>) => {
+              setSelectFrom(e.target.value)
+            }}
+            className='border-b py-1 text-lg font-medium text-[#676767] w-64 h-8 border-[#005687]'
+          >
+            {stateName?.map((item: any, index) => {
+              return (<option value={item.state_name} key={index}>{item.state_name}</option>)
+            })}
+          </select>
         </div>
+
+        <div>
+          <h1 className='font-semibold text-md text-[#676767] pt-6'>To</h1>
+          <select value={selectTo} onChange={(e: React.ChangeEvent<HTMLSelectElement>) => { setSelectTo(e.target.value) }}
+            className='border-b py-1 text-lg font-medium text-[#676767] w-64 h-8 border-[#005687]'
+          >
+            {stateName?.map((item: any, index) => {
+              return (<option value={item.state_name} key={index}>{item.state_name}</option>)
+            })}
+          </select>
+        </div>
+
+        <h1 className='font-semibold text-md text-[#676767] pt-6 -ml-4'>Methods</h1>
+        <div className='flex gap-x-2 pt-4'>
+
+          <label className={`flex flex-col items-center justify-center border w-20 h-14 rounded-lg border-[#005687] overflow-hidden ${selectedMethods.includes('bulkabus') ? 'bg-[#005687] border-2 border-black' : 'bg-[#e2e1e1]'}`}>
+            <input onChange={OnTarvelModeSelected} type="checkbox" value="bulkabus" className=' hidden checked:bg-black' />
+            <Image alt='bulkabus' src="./bulka_bus.svg" height={20} width={20} className='h-14 w-16 mt-7 ' />
+          </label>
+
+          <label className={`flex flex-col items-center justify-center border w-20 h-14 rounded-lg border-[#005687] overflow-hidden ${selectedMethods.includes('Car') ? 'bg-[#005687] border-2 border-black' : 'bg-[#e2e1e1]'}`}>
+            <input onChange={OnTarvelModeSelected} type="checkbox" value="Car" className=' hidden checked:bg-black' />
+            <Image alt='car' src="./car.svg" height={20} width={20} className='h-14 w-16 mt-7 ' />
+          </label>
+
+          <label className={`flex flex-col items-center justify-center border w-20 h-14 rounded-lg border-[#005687] overflow-hidden ${selectedMethods.includes('Bike') ? 'bg-[#005687] border-2 border-black' : 'bg-[#e2e1e1]'}`}>
+            <input onChange={OnTarvelModeSelected} type="checkbox" value="Bike" className=' hidden checked:bg-black' />
+            <Image alt='bulkabus' src="./bike.svg" height={20} width={20} className='h-14 w-16 mt-7 ' />
+          </label>
+        </div>
+        <button type='submit' className='border-2 mt-12 p-3 w-40 border-black bg-[#005687] text-white  rounded-lg '>Search Trips</button>
+      </form>
     </div>
     
   )
